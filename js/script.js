72f8fd7ca268a0e3b2b7a4ea0ebe3ff108ac6816
sarjid <sarjid@ssd-tech.io>
2021-01-29 23:55:20 +0600

    added terminal

--- js/script.js
+++ js/script.js
@@ -1,24 +1,43 @@
+
+
 function yellow(message) {
-return "[[;green;black]" + message + "]";
+    return "[[;green;black]" + message + "]";
 }
 
-function callback(response) {
+function type(response) {
 
     var app = document.getElementById('app');
     var typewriter = new Typewriter(app, {
-    loop: false ,
-    delay: 100
+        loop: false,
+        delay: 80,
+        cursor: ''
     });
     typewriter.typeString(response)
-              .start();
+        .start()
+        .callFunction(terminal);
+}
+$.ajax({
+    url: 'intro.txt',
+    success: function (data) {
+        type(data);
+    }
+});
+
+function terminal(){
+
+    $('#terminal').terminal({
+        hello: function () {
+            this.echo('Hello, visitor' +
+                '. Wellcome to this terminal.');
+        }
+    }, {
+        checkArity: false,
+        greetings: '',
+        prompt: '$'
+    });
+
+
 }
 
-$.ajax({
-    url:'intro.txt',
-    success: function (data){
-        callback(data);
-    }
-  });
 
 
-    
