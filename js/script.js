0cfae30d4e87791b8f75dafcd416a8d33908e07b
sarjid <sarjid@ssd-tech.io>
2021-02-12 03:17:26 +0600

    added new terminal

--- js/script.js
+++ js/script.js
@@ -1,43 +1,41 @@
+$(function() {
 
-
-function yellow(message) {
-    return "[[;green;black]" + message + "]";
-}
+    $.ajax({
+        url: 'intro.txt',
+        success: function(data) {
+            type(data);
+        }
+    });
+});
 
 function type(response) {
-
     var app = document.getElementById('app');
     var typewriter = new Typewriter(app, {
         loop: false,
-        delay: 80,
+        delay: 45,
         cursor: ''
     });
     typewriter.typeString(response)
         .start()
         .callFunction(terminal);
 }
-$.ajax({
-    url: 'intro.txt',
-    success: function (data) {
-        type(data);
-    }
-});
 
-function terminal(){
+function terminal() {
 
-    $('#terminal').terminal({
-        hello: function () {
-            this.echo('Hello, visitor' +
-                '. Wellcome to this terminal.');
-        }
-    }, {
-        checkArity: false,
-        greetings: '',
-        prompt: '$'
-    });
-
-
+    setTimeout(function() {
+        $('#terminal').addClass('term');
+        $('#terminal').terminal({
+            help: function() {
+                this.echo(color(res));
+            }
+        }, {
+            checkArity: false,
+            greetings: false,
+            prompt: color('>')
+        });
+    }, 500);
 }
 
-
-
+function color(message) {
+    return "[[;#00FF41;black]" + message + "]";
+}
---
+++ js/terminal_response.js
@@ -0,0 +1,1 @@
+var res = "hello ,visitor.how are you?";
